796856128edb9de7a7d6d59aedd3493242a50325
J.Gaschina <jan.gaschina@tu-dortmund.de>
2023-05-11 17:37:29 +0200

    Update VXI11test.py

--- VXI11test.py
+++ VXI11test.py
@@ -1,4 +1,5 @@
 import vxi11
+import time
 instr=vxi11.Instrument("129.217.164.90")
 instr.write("LAMP 0,3.3")
 instr.write("LAMP 1,3.3")
@@ -8,11 +9,12 @@ instr.write("LAMP 4,3.3")
 instr.write("DLAY 4,2,250e-9")
 befehl1="DLAY "
 komma=","
-zpotenz="e-12"
+zpotenz="e-9"
 i=0
-start=250000
-stop=380000
-step=50
+start=250
+stop=800
+step=10e-3
+Delay="DISP 11,4"
 flanke1="1"
 flanke2="2"
 flanke3="3"
@@ -28,4 +30,7 @@ flanke10="10"
 while i<=(stop-start)/step:
     wert=str(start+i*step)
     pck=befehl1 + flanke4 + komma + flanke2 + komma + wert + zpotenz
+    instr.write(pck)
+    instr.write(Delay)
+    time.sleep(1)
     i=i+1
