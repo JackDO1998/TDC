23c3633d5de0ba3587c4359610147362a4c48429
J.Gaschina <jan.gaschina@tu-dortmund.de>
2023-05-13 00:48:51 +0200

    Update burstmodetest.py

--- burstmodetest.py
+++ burstmodetest.py
@@ -35,26 +35,23 @@ try:
             TIME2=tdc.read24(0x12)
             TIME3=tdc.read24(0x14)
             TIME4=tdc.read24(0x16)
+            TIME5=tdc.read24(0x18)
             COUNT1=tdc.read24(0x11)
             COUNT2=tdc.read24(0x13)
             COUNT3=tdc.read24(0x15)
             COUNT4=tdc.read24(0x17)
-            #calCount=(CALIBRATION2-CALIBRATION1)/(CALIBRATION_PERIODS-1)
-            #normalLSB=1/(calCount*CLOCK)
+            calCount=(CALIBRATION2-CALIBRATION1)/(CALIBRATION_PERIODS-1)
+            normalLSB=1/(calCount*CLOCK)
             
-            #TOF1=TIME1*normalLSB
-            #TOF2=TIME2*normalLSB
-            #TOF3=TIME3*normalLSB
-            #TOF4=TIME4*normalLSB
+            TOF1=TIME1*normalLSB + COUNT1/CLOCK - (TIME2*normalLSB)
+            TOF2=TIME2*normalLSB + COUNT2/CLOCK - (TIME3*normalLSB)
+            TOF3=TIME3*normalLSB + COUNT3/CLOCK - (TIME4*normalLSB)
+            TOF4=TIME4*normalLSB + COUNT4/CLOCK - (TIME5*normalLSB)
 
-            print(TIME1)
-            print(COUNT1)
-            print(TIME2)
-            print(COUNT2)
-            print(TIME3)
-            print(COUNT3)
-            print(TIME4)
-            print(COUNT4)
+            print(TOF1)
+            print(TOF2)
+            print(TOF3)
+            print(TOF4)
 
             #times.append(TOF1*1e9)
             #times.append(TOF2*1e9)
