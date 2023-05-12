c43b64a282efb523bb4406be5f62c8523cf8ad22
J.Gaschina <jan.gaschina@tu-dortmund.de>
2023-05-12 15:16:14 +0200

    Update tdc7201test.py

--- tdc7201test.py
+++ tdc7201test.py
@@ -40,7 +40,7 @@ pfad="histogramme/"
 titel=t1+str(start+i*step)+t2+t3+str(x)+t4
 saveas=pfad+str(start+i*step)+t2+endung
 try:
-    pck=befehl1 + flanke4 + komma + flanke2 + komma + start + zpotenz
+    pck=befehl1 + flanke4 + komma + flanke2 + komma + str(start) + zpotenz
     instr.write(pck)
     while i<=(stop-start)/step:
         status = tdc.measure(simulate=False)
