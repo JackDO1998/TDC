66cba05739d40dd0bf6088d54cda96c5197a7653
J.Gaschina <jan.gaschina@tu-dortmund.de>
2023-05-12 20:52:21 +0200

    Update tdc7201mod1.py

--- tdc7201mod1.py
+++ tdc7201mod1.py
@@ -9,7 +9,7 @@ tdc = tdc7201.TDC7201()
 tdc.initGPIO(enable=11, osc_enable=15, trig1=7, int1=29, trig2=None, int2=None, verbose=True, start=None, stop=None)
 tdc.set_SPI_clock_speed(1250000)
 tdc.on()
-tdc.configure(meas_mode=1,num_stop=1, trig_falling=False, calibration2_periods=40)
+tdc.configure(meas_mode=1,num_stop=1, trig_falling=False, calibration2_periods=10)
 
 instr=vxi11.Instrument("129.217.164.90")
 instr.write("LAMP 0,3.3")
@@ -52,7 +52,7 @@ try:
             CALIBRATION2=tdc.read24(0x1C)
             TIME1=tdc.read24(0x10)
             CLOCK=8e6
-            CALIBRATION_PERIODS=40
+            CALIBRATION_PERIODS=10
             calCount=(CALIBRATION2-CALIBRATION1)/(CALIBRATION_PERIODS-1)
             normalLSB=1/(calCount*CLOCK)
             TOF=TIME1*normalLSB
