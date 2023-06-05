d81ff8c7fe2d3811844a65ee7fc477361768f495
J.Gaschina <jan.gaschina@tu-dortmund.de>
2023-06-05 19:28:47 +0200

    verschiedenes

--- auswertung.py
+++ auswertung.py
@@ -6,37 +6,54 @@ Created on Mon May 15 23:15:53 2023
 """
 import matplotlib.pyplot as plt
 import numpy as np
-a=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/mittelwerteLED.txt', usecols=[0], dtype=float)
-b=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/mittelwerteLED.txt', usecols=[1], dtype=float)
-c=b-a
-plt.plot(a,c,'r*')
+a1=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-400ns/mittelwerteLED.txt', usecols=[0], dtype=float)
+b1=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-400ns/mittelwerteLED.txt', usecols=[1], dtype=float)
+c1=b1-a1
+a2=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-50ns/mittelwerteLED.txt', usecols=[0], dtype=float)
+b2=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-50ns/mittelwerteLED.txt', usecols=[1], dtype=float)
+c2=b2-a2
+a3=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-50ns@250ps/mittelwerteLED.txt', usecols=[0], dtype=float)
+b3=np.loadtxt('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-50ns@250ps/mittelwerteLED.txt', usecols=[1], dtype=float)
+c3=b3-a3
+a=[]
+c=[]
+z=0
+while z <= len(a1)-1:
+    a.append(a1[z])
+    c.append(c1[z])
+    z=z+1
+plt.plot(a,c,'r*',label='250 ps')
+plt.plot(a2,c2,'b*',label='100 ps')
+plt.plot(a3,c3,'g*',label='250 ps')
 plt.grid(True)
+plt.legend(loc='best')
 plt.xlabel("Eingestellte Zeit in ns")
 plt.ylabel('Abweichung')
 plt.title('Gesamtplot')
-plt.savefig('Gesamtplot norm.pdf')
+plt.savefig('D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-400ns/Gesamtplot kombiniert 2 norm.pdf')
 plt.close()
-j=0
-h=1
-while h<=25:
-    a1=[]
-    b1=[]
-    while j<=(h*100)-1:
-        a1.append(a[j])
-        b1.append(c[j])
-        j=j+1
-    name="zoomplot nr "
-    nr=str(h)
-    endung="a.pdf"
-    plotname=name+nr+endung
-    titel=name+nr
-    plt.plot(a1,b1,'k-')
-    plt.plot(a1,b1,'r*')
-    plt.xlabel("Eingestellte Zeit in ns")
-    plt.ylabel('relative Abweichung')
-    plt.grid(True)
-    plt.title(titel)
-    plt.savefig(plotname)
-    plt.close()
+
+#j=0
+#h=1
+#while h<=25:
+#    a1=[]
+#    b1=[]
+#    while j<=(h*100)-1:
+#        a1.append(a[j])
+#        b1.append(c[j])
+#        j=j+1
+#    name="D:/OneDrive/Dokumente/Bachelorarbeit/MittelwertzoomplotsLED/7-400ns/zoomplot nr "
+#   nr=str(h)
+#    endung="a.pdf"
+#    plotname=name+nr+endung
+#    titel=name+nr
+#    plt.plot(a1,b1,'k-')
+#    plt.plot(a1,b1,'r*')
+#    plt.xlabel("Eingestellte Zeit in ns")
+#   plt.ylabel('relative Abweichung')
+#    plt.grid(True)
+#    plt.title(titel)
+    #plt.savefig(plotname)
+#    plt.close()
         
-    h=h+1
+#    h=h+1
--- fuellstruktur.py
+++ fuellstruktur.py
@@ -1,14 +1,29 @@
-import multiprocessing
-
 from multiprocessing import Process, Queue
 import channel_access.common as ca
 import channel_access.server as cas
+import tdc7201
+
+
+tdc = tdc7201.TDC7201()
+tdc.initGPIO(enable=11, osc_enable=15, trig1=7, int1=29, trig2=None, int2=None, verbose=True, start=None, stop=None)
+tdc.set_SPI_clock_speed(1250000)
+tdc.on()
+tdc.configure(meas_mode=1,num_stop=1, trig_falling=False, calibration2_periods=10)
+tdc.write8(0x00,0x01)
 
 def read_spi(q):
-    value=0
-    while True:
-        value = value + 1
-        q.put(value)
+        status = tdc.measure(simulate=False)
+        if status == 1:
+            CALIBRATION1=tdc.read24(0x1B)
+            CALIBRATION2=tdc.read24(0x1C)
+            TIME1=tdc.read24(0x10)
+            CLOCK=8e6
+            CALIBRATION_PERIODS=10
+            calCount=(CALIBRATION2-CALIBRATION1)/(CALIBRATION_PERIODS-1)
+            normalLSB=1/(calCount*CLOCK)
+            TOF=TIME1*normalLSB
+    
+        q.put(TOF)
 
 if __name__ == '__main__':
     q = Queue()
