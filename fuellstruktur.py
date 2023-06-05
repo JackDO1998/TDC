8841a4a894317ea307ae4261486704eb5a699df6
J.Gaschina <jan.gaschina@tu-dortmund.de>
2023-06-05 15:04:11 +0200

    Update fuellstruktur.py

--- fuellstruktur.py
+++ fuellstruktur.py
@@ -1,3 +1,30 @@
 import multiprocessing
 
-print("Number of CPUs : ",multiprocessing.cpu_count())
+from multiprocessing import Process, Queue
+import channel_access.common as ca
+import channel_access.server as cas
+
+def read_spi(q):
+    value=0
+    while True:
+        value = value + 1
+        q.put(value)
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=read_spi, args=(q,))
+    p.start()
+
+    with cas.Server() as server:
+        pv = server.createPV('GAS-buckets', ca.Type.FLOAT, count=192)
+
+        values = []
+        while True:
+            values.append(q.get())
+            if len(values) > 8000:
+                buckets = values
+                values = []
+                pv.put(buckets)
+
+    p.join()
+ 
